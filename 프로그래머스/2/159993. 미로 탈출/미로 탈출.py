6824bd8e5af1dd61651b9bf17866649762cbd119
김보빈 <125635310+k-bobin@users.noreply.github.com>
2023-10-10 01:54:32 +0900

    [level 2] Title: 미로 탈출, Time: 0.04 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.py
@@ -0,0 +1,39 @@
+
+from collections import deque
+def solution(maps):
+    answer = 0
+    direction = [[0,1],[0,-1],[1,0],[-1,0]]
+    n,m = len(maps),len(maps[0])
+    # 출발 지점
+    for i in range(n):
+        for j in range(m):
+            if maps[i][j] == 'S':
+                sx,sy = i,j
+    # 레버 찾기
+    def bfs(x,y,end):
+        q = deque()
+        q.append([x,y])
+        visited = [[-1]*m for _ in range(n)]
+        visited[x][y] = 0
+        while q:
+            x,y = q.popleft()
+            if maps[x][y] == end:
+                return [visited[x][y],x,y]
+            for dir in direction:
+                nx = x + dir[0]
+                ny = y + dir[1]
+                if 0 <= nx < n and 0 <= ny < m:
+                    if visited[nx][ny] == -1:
+                        if maps[nx][ny] != 'X':
+                            q.append([nx,ny])
+                            visited[nx][ny] = visited[x][y] + 1
+        return None
+    cnt = bfs(sx,sy,'L')
+    if cnt == None:
+        return -1
+    answer += cnt[0]
+    cnt = bfs(cnt[1],cnt[2],'E')
+    if cnt == None:
+        return -1
+    answer += cnt[0]
+    return answer
