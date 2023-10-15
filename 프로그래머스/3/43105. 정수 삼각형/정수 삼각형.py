2370c8f81f4314275e2d13582f2dfe1ccdb77a5b
김보빈 <125635310+k-bobin@users.noreply.github.com>
2023-10-15 22:09:59 +0900

    [level 3] Title: 정수 삼각형, Time: 43.67 ms, Memory: 14.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,11 @@
+def solution(triangle):
+
+    height = len(triangle)
+
+    while height > 1:
+        for i in range(height - 1):
+            triangle[height-2][i] += max([triangle[height-1][i], triangle[height-1][i+1]])
+        height -= 1
+
+    answer = triangle[0][0]
+    return answer
