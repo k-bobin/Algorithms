a4984bd30d4b0cea0d7907664fc11c585eb7ad8e
김보빈 <125635310+k-bobin@users.noreply.github.com>
2023-10-17 15:55:28 +0900

    [level 3] Title: 기지국 설치, Time: 8.31 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/12979. 기지국 설치/기지국 설치.py
@@ -0,0 +1,23 @@
+#1:36
+import math
+
+def solution(n, stations, w):
+    answer = 0
+    s = 1
+
+    for station in stations:
+        e = station - w
+        print(s,e)
+        total = e - s
+        if e > 1 and total > 0:
+            answer += math.ceil(total / ((2 * w) + 1))
+
+        s = station + w + 1
+
+        
+        
+    if s <= n:
+        total = (n + 1) - s
+        answer += math.ceil(total / ((2 * w) + 1))
+
+    return answer
