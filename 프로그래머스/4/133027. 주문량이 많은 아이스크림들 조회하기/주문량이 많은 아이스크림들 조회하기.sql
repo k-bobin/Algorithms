c92d46aa81adfe028930ee29a2517563e0f194ad
김보빈 <125635310+k-bobin@users.noreply.github.com>
2023-11-10 00:19:44 +0900

    [level 4] Title: 주문량이 많은 아이스크림들 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
+++ 프로그래머스/4/133027. 주문량이 많은 아이스크림들 조회하기/주문량이 많은 아이스크림들 조회하기.sql
@@ -1,12 +1,13 @@
-select p.flavor
-    from (
-    (select f.flavor, sum(f.total_order) as total_order 
-        from first_half as f
-    group by f.flavor) 
-    union
-    (select j.flavor, sum(j.total_order) as total_order 
-        from july as j
-    group by j.flavor)) as p
-group by flavor
-order by sum(p.total_order) desc
-limit 3
+SELECT flavor
+FROM
+    (
+        SELECT *
+        FROM july
+        UNION ALL
+        SELECT *
+        FROM first_half
+    ) A
+GROUP BY flavor
+ORDER BY SUM(total_order) DESC
+LIMIT 3
+;
