0c65a55c57b68824afb333069ece614df0aedfec
김보빈 <125635310+k-bobin@users.noreply.github.com>
2023-11-10 03:35:27 +0900

    [level 4] Title: 그룹별 조건에 맞는 식당 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
+++ 프로그래머스/4/131124. 그룹별 조건에 맞는 식당 목록 출력하기/그룹별 조건에 맞는 식당 목록 출력하기.sql
@@ -1,14 +1,21 @@
-select member_name, review_text, date_format(review_date,"%Y-%m-%d")
-    from rest_review as r
-        join (select a.member_id
-                ,m.member_name
-                ,rank() over(order by reviewcount desc) as ranking
-                    from(select *
-                                ,count(member_id) as reviewcount
-                            from rest_review
-                            group by rest_review.member_id) as a
-                    join member_profile as m
-                    on a.member_id=m.member_id) as c
-        on r.member_id = c.member_id
-where ranking = 1
-order by r.review_date,r.review_text
+-- 코드를 입력하세요
+WITH selected_id AS (
+SELECT member_id
+FROM
+    (SELECT member_id
+            ,count(*) as review_cnt
+            ,rank() over (order by count(*) desc) as ranking
+    FROM rest_review
+    GROUP BY member_id) AS rank_table
+WHERE ranking = 1
+)
+
+SELECT member_profile.member_name,
+        review_text,
+        date_format(review_date, '%Y-%m-%d') as review_date
+FROM rest_review 
+LEFT JOIN member_profile 
+on rest_review.member_id = member_profile.member_id
+WHERE rest_review.member_id in (SELECT member_id 
+                    FROM selected_id)
+ORDER BY 3,2
